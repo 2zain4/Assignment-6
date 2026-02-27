3bc171227a9192d82aa5ae9954d595a516d01a64
2zain4 <zain23133@gmail.com>
2026-02-27 02:11:41 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import bootstrap from "./app.controller.js"
 
 import { sequelize } from "./DB/connectionDB.js";
-import "./DB/models/index.js";   // 👈 مهم جداً
+import "./DB/models/index.js";   
 
 
 
